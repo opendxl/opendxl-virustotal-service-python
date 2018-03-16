a51877147f21a6ed6e794dcb4f12bbd863cfbd8e
Jeremy Barlow <jeremy_barlow@mcafee.com>
2018-03-16 11:15:45 -0700

    Update pylint glob to parse all sample files

--- sample/basic/basic_url_report_example.py
+++ sample/basic/basic_url_report_example.py
@@ -9,7 +9,7 @@ import sys
 
 from dxlclient.client_config import DxlClientConfig
 from dxlclient.client import DxlClient
-from dxlclient.message import Message, Event, Request
+from dxlclient.message import Message, Request
 from dxlbootstrap.util import MessageUtils
 
 # Import common logging and configuration
